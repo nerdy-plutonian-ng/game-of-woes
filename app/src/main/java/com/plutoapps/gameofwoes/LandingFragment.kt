1549594ef11910b34fc5778aaff24581fa100475
nerdyplutonian <nerdymykl@gmail.com>
2022-02-18 06:47:30 +0000

    initial commit

---
+++ app/src/main/java/com/plutoapps/gameofwoes/LandingFragment.kt
@@ -0,0 +1,35 @@
+package com.plutoapps.gameofwoes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.findNavController
+import com.plutoapps.gameofwoes.databinding.FragmentLandingBinding
+
+class LandingFragment : Fragment() {
+
+    private lateinit var binding : FragmentLandingBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = FragmentLandingBinding.inflate(layoutInflater,container,false)
+        return  binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.button.setOnClickListener {
+            view.findNavController().navigate(R.id.action_landingFragment_to_choicesFragment)
+        }
+    }
+}
